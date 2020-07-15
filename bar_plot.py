afcc2ff983039612fcb7a2a764d21fc136a73e72
Miguel <miguelcastellanomerino@gmail.com>
2020-07-15 11:09:10 +0200

    new runs hawk

--- bar_plot.py
+++ bar_plot.py
@@ -3,55 +3,42 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
+apio =  [12.8363, 20.1325, 19.1207, 22.4712, 20.3637, 19.1664, 19.6664, 18.3873, 20.3834, 19.2077, 23.3460]
 
-nprocs = ['24', '48', '96', '192', '384','768']
-
-apio_1T =     [118.8, 72.5, 48.9, 59.5, 76.2, 0]
-original_1T = [161.9, 79.7, 51.7, 56.5, 75.2, 0]
-
-apio_2T =     [100.3, 56.8, 35.0, 27.8, 0, 0]
-original_2T = [143.5, 59.4, 37.3, 29.2, 0, 0]
-
-apio_4T =     [92.7, 46.1, 27.8, 19.6, 0, 0]
-original_4T = [136.1, 47.3, 27.9, 19.7, 0, 0]
-
-apio_10T =     [88.1, 41.8, 22.5, 12.2, 0, 0]
-original_10T = [129.7, 42.3, 22.7, 12.7, 0, 0]
+error= [0.540572, 1.53785, 7.21405, 5.5195, 5.31839, 2.08903, 5.93908, 2.16524, 1.91775, 2.5841, 3.11317]
 
 
-apio = apio_10T
-original = original_10T
-
-
-x = np.arange(len(nprocs))  # the label locations
+x = np.arange(len(apio))  # the label locations
 width = 0.35  # the width of the bars
 
 fig, ax = plt.subplots(figsize=(8,8))
-rects1 = ax.bar(x - width/2, apio, width, label='APIO')
-rects2 = ax.bar(x + width/2, original, width, label='ORIGINAL')
+rects1 = ax.bar(x,apio, width,yerr=error)
+#rects2 = ax.bar(x + width/2, original, width, label='ORIGINAL')
 
 # Add some text for labels, title and custom x-axis tick labels, etc.
-ax.set_ylabel('Time (s) [averaged over 10 runs]')
-ax.set_xlabel('ntasks [MPI procs]')
-ax.set_title('Runtime savings for the APIO asynchronous pipeline (10\u0394t)')
+ax.set_ylabel('Speedup (%) [averaged over 12 hour run]')
+ax.set_xlabel('Runs')
+ax.set_title('Speedup for APIO on one node (128 cores) - 16M cells (1\u0394t)')
 ax.set_xticks(x)
-ax.set_xticklabels(nprocs)
-ax.legend()
+#ax.set_xticklabels(apio)
+#ax.legend()
 
 
-def autolabel(rects1,rects2):
+def autolabel(rects1):
     """Attach a text label above each bar in *rects*, displaying its height."""
-    for rect1,rect2 in zip(rects1,rects2):
-        height = max(rect1.get_height(),rect2.get_height())
-        gain = (rect2.get_height() - rect1.get_height())*100/rect2.get_height()
-        ax.annotate('{}%'.format(round(gain,3)),
-                    xy=(rect1.get_x() + rect1.get_width() , height),
-                    xytext=(0, 5),  # 5 points vertical offset
+    i=0
+    for rect in rects1:
+        height = rect.get_height() 
+        err = error[i]
+        ax.annotate('{} %'.format(height),
+                    xy=(rect.get_x() + rect.get_width() , height),
+                    xytext=(0, 18*err),  # 5 points vertical offset
                     textcoords="offset points",
                     ha='center', va='bottom')
+        i+=1
 
 
-autolabel(rects1,rects2)
+autolabel(rects1)
 
 fig.tight_layout()
 
